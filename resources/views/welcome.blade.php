0ae840734373445399395b6082da473b6a1fc891
Hiyaro <nawfal.elkhaznagi@gmail.com>
2022-09-27 16:48:22 +0200

    fin de journée

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>projet executant</title>
+    @vite('resources/css/app.css')
 </head>
-<body>
+<body >
 
 
-    this is just me having fun
+    @if (Route::has('login'))
+    <div class="hidden sm:block">
+        @auth
+            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 no-underline hover:underline">Dashboard</a>
+        @else
+
+<div class="flex flex-col">
+
+    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 no-underline hover:underline">Log in</a>
+
+    @if (Route::has('register'))
+    <a href="{{ route('register') }}" class="text-sm text-gray-700 dark:text-gray-500 no-underline hover:underline">Register</a>
+
+</div>
+            @endif
+        @endauth
+    </div>
+@endif
 </body>
 </html>
